52432d877273940179d0a560ffb4db677160e490
ky-lan <113309181+ky-lan@users.noreply.github.com>
2023-05-17 23:03:27 -0400

    working on footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,20 +4,39 @@ import './Footer.scss';
 export default function Footer() {
   return (
     <>
-      <div className='footer-links'>
-        <div className="terms-link">
-          <Link to="/terms">
-            Terms of Use
-          </Link>
+      <div className="bgFooter">
+        <div className='footer-logo'>
+          <nav>
+            © 2023
+            <div className="bgFooterLogo">
+              <Link to="/" className='logo-text1'>
+                CHEW
+              </Link>
+              <Link to="/" className='logo-text2'>
+                namic
+              </Link>
+              All Rights Reserved
+            </div>
+          </nav>
         </div>
-        <div className="privacy-link">
-          <Link to="/privacy">
-            Privacy Policy
-          </Link>
+      
+
+
+        <div className='footer-links'>
+          <div className="terms-link">
+            <Link to="/terms">
+              Terms of Use
+            </Link>
+          </div>
+          <div className="privacy-link">
+            <Link to="/privacy">
+              Privacy Policy
+            </Link>
+          </div>
         </div>
-      </div>
-      <div className="copyright">
-        Copyright Pulpy Seed, 2023
+        <div className="copyright">
+          Copyright Pulpy Seed, 2023
+        </div>
       </div>
     </>
   );
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -16,6 +16,17 @@ export default function Home() {
         </div>
       </div>
       <div className='home-products-area-top'>
+        <div className='gum-images-row1'>
+          <div className="banana-cherry-img-link">
+            <img src="images/Banana_to_Cherry1.png" className="banana-cherry-img" alt="Orange Mango Bottle" width="460" height="460" />
+          </div>
+          <div className="peppermint-lemon-img-link">
+            <img src="images/Pepperm_to_Lemon1.png" className="peppermint-lemon-img" alt="Apple Strawberry Bottle" width="460" height="460" />
+          </div>
+          <div className="raspberry-peppermint-img-link">
+            <img src="images/Rasp_to_Pepperm1.png" className="raspberry-peppermint-img" alt="Mango Strawberry Bottle" width="460" height="460" />
+          </div>
+        </div>
         <div className='names-of-gum-row1'>
           <div className='banana-cherry-label'>
             Banana to Cherry
@@ -27,19 +38,31 @@ export default function Home() {
             Raspberry to Peppermint
           </div>
         </div>
-        <div className='bottle-images-row1'>
-          <div className="banana-cherry-img-link">
-            <img src="images/Banana_to_Cherry1.png" className="banana-cherry-img" alt="Orange Mango Bottle" width="460" height="460" />
+        <div className='home-explore-btns'>
+          <div className='home-explore-btn1'>
+            <button>Explore</button>
           </div>
-          <div className="peppermint-lemon-img-link">
-            <img src="images/Pepperm_to_Lemon1.png" className="peppermint-lemon-img" alt="Apple Strawberry Bottle" width="460" height="460" />
+          <div className='home-explore-btn2'>
+            <button>Explore</button>
           </div>
-          <div className="raspberry-peppermint-img-link">
-            <img src="images/Rasp_to_Pepperm1.png" className="raspberry-peppermint-img" alt="Mango Strawberry Bottle" width="460" height="460" />
+          <div className='home-explore-btn3'>
+            <button>Explore</button>
           </div>
         </div>
+        
       </div>
     </div>
+      <div className='gum-images-row2'>      
+        <div className="cherry-lemon-img-link">
+          <img src="images/Cherry_to_Lemon1.png" className="cherry-lemon-img" alt="Apple Grape Bottle" width="460" height="460" />
+        </div>
+        <div className="banana-peppermint-img-link">
+          <img src="images/Banana_to_Pepperm1.png" className="banana-peppermint-img" alt="Grape Strawberry Bottle" width="460" height="460" />
+        </div>
+        <div className="peppermint-cherry-img-link">
+          <img src="images/Pepperm_to_Cherry1.png" className="peppermint-cherry-img" alt="Grape Strawberry Bottle" width="460" height="460" />
+        </div>
+      </div>
       <div className='names-of-gum-row2'>
         <div className='cherry-lemon-label'>
           Cherry to Lemon
@@ -51,28 +74,10 @@ export default function Home() {
           Peppermint to Cherry
         </div>
       </div>
-      <div className='bottle-images-row2'>      
-        <div className="cherry-lemon-img-link">
-          <img src="images/Cherry_to_Lemon1.png" className="cherry-lemon-img" alt="Apple Grape Bottle" width="460" height="460" />
-        </div>
-        <div className="banana-peppermint-img-link">
-          <img src="images/Banana_to_Pepperm1.png" className="banana-peppermint-img" alt="Grape Strawberry Bottle" width="460" height="460" />
-        </div>
-        <div className="peppermint-cherry-img-link">
-          <img src="images/Pepperm_to_Cherry1.png" className="peppermint-cherry-img" alt="Grape Strawberry Bottle" width="460" height="460" />
-        </div>
-      </div>
+      
 
       <div className='add-to-cart-btn-container'>
-        <div className='add-to-cart-btn1'>
-            <button>Add to Cart</button>
-          </div>
-        <div className='add-to-cart-btn2'>
-          <button>Add to Cart</button>
-        </div>
-        <div className='add-to-cart-btn3'>
-          <button>Add to Cart</button>
-        </div>
+        
         <div className='add-to-cart-btn4'>
           <button>Add to Cart</button>
         </div>
