dc7ad910abab88a85a155fb418e7d91d173478a0
ky-lan <113309181+ky-lan@users.noreply.github.com>
2023-05-19 18:14:01 -0400

    display block not working on custom page for flavour 2

--- src/components/Custom/index.js
+++ src/components/Custom/index.js
@@ -12,35 +12,69 @@ export default function Custom(props) {
         <div className='custom-body'>
 
 
+          <div className='custom-fieldsets'>
+            <fieldset className='flavour1-fieldset'>
+              <legend>Flavour 1</legend>
 
-          <fieldset className='flavour1-fieldset'>
-            <legend>Flavour 1</legend>
+              <div>
+                <input type="radio" id="banana" name="flavour1" value="banana" />
+                <label htmlFor="banana">Banana</label>
+              </div>
 
-            <div>
-              <input type="radio" id="banana" name="flavour1" value="banana" />
-              <label htmlFor="banana">Banana</label>
-            </div>
+              <div>
+                <input type="radio" id="cherry" name="flavour1" value="cherry" />
+                <label htmlFor="cherry">Cherry</label>
+              </div>
 
-            <div>
-              <input type="radio" id="cherry" name="flavour1" value="cherry" />
-              <label htmlFor="cherry">Cherry</label>
-            </div>
+              <div>
+                <input type="radio" id="peppermint" name="flavour1" value="peppermint" />
+                <label htmlFor="peppermint">Peppermint</label>
+              </div>
 
-            <div>
-              <input type="radio" id="peppermint" name="flavour1" value="peppermint" />
-              <label htmlFor="peppermint">Peppermint</label>
-            </div>
+              <div>
+                <input type="radio" id="lemon" name="flavour1" value="lemon" />
+                <label htmlFor="lemon">Lemon</label>
+              </div>
 
-            <div>
-              <input type="radio" id="lemon" name="flavour1" value="lemon" />
-              <label htmlFor="lemon">Lemon</label>
-            </div>
+              <div>
+                <input type="radio" id="raspberry" name="flavour1" value="raspberry" />
+                <label htmlFor="raspberry">Raspberry</label>
+              </div>
+            </fieldset>
 
-            <div>
-              <input type="radio" id="raspberry" name="flavour1" value="raspberry" />
-              <label htmlFor="raspberry">Raspberry</label>
-            </div>
-          </fieldset>
+            <fieldset className='flavour2-fieldset'>
+              <legend>Flavour 2</legend>
+
+              <div>
+                <input type="radio" id="banana" name="flavour2" value="banana" />
+                <label htmlFor="banana">Banana</label>
+              </div>
+
+              <div>
+                <input type="radio" id="cherry" name="flavour2" value="cherry" />
+                <label htmlFor="cherry">Cherry</label>
+              </div>
+
+              <div>
+                <input type="radio" id="peppermint" name="flavour2" value="peppermint" />
+                <label htmlFor="peppermint">Peppermint</label>
+              </div>
+
+              <div>
+                <input type="radio" id="lemon" name="flavour2" value="lemon" />
+                <label htmlFor="lemon">Lemon</label>
+              </div>
+
+              <div>
+                <input type="radio" id="raspberry" name="flavour2" value="raspberry" />
+                <label htmlFor="raspberry">Raspberry</label>
+              </div>
+            </fieldset>
+          </div>
+
+          <div className='custom-explanation'>
+            <p>After about 10 minutes of chewing, your gum will change flavour from flavour 1 to flavour 2.</p>
+          </div>
 
 
 
