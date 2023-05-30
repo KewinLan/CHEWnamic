a5733cefadc0dfc790aa1cd3c6871e256942370e
ky-lan <113309181+ky-lan@users.noreply.github.com>
2023-05-30 16:58:25 -0400

    cart page's product 1 is done, do rest of cart page

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -1,33 +1,50 @@
 import React from 'react';
-import AuthDetails from '../auth/AuthDetails/AuthDetails';
 import './Cart.scss';
-import SignIn from '../auth/SignIn/SignIn';
-import SignUp from '../auth/SignUp/SignUp';
 
 export default function Cart() {
   return (
     <div className='cart-everything'>
       <div className='cart-title'>
-        <h1>Your Cart</h1>
+        <h1>Cart (6 items)</h1>
       </div>
-      <div className="account-section">
-        <div className='sign-in-section'>
-          <div className='sign-in-head2'>
-            <h2>Sign In</h2>
-          </div>
-          <br></br>
-          <SignIn />
+      <div className='cart-product-names'>
+        <div className='cart-product-name1'>
+          <p>Peppermint to Lemon</p>
         </div>
-        <div className='sign-up-section'>
-          <div className='sign-up-head2'>
-            <h2>Sign Up</h2>
-          </div>
-          <br></br>
-          <SignUp />
+        <div className='cart-product-name2'>
+          <p>Banana to Peppermint</p>
+        </div>
+        <div className='cart-product-name3'>
+          <p>Cherry to Lemon</p>
         </div>
       </div>
-      <div className='authdetails-section'>
-        <AuthDetails />
+      <div className='cart-carousel'>
+        <div className='cart-carousel-l-arrow'>
+          <img src="images/L-arrow.png" className="cart-carousel-l-arrow-class" alt="Previous" width="26" height="42" />
+        </div>
+        <div className='cart-carousel-item1'>
+          <div className='cart-carousel-item1-img'>
+            <img src="images/Pepperm_to_Lemon1.png" className="cart-carousel-item1-img-class" alt="Peppermint to Lemon" width="332" height="261" />
+          </div>
+          <div className='cart-carousel-item1-info'>
+            <div className='cart-carousel-item1-info-price'>
+              <p>$1.99</p>
+            </div>
+            <div className='cart-carousel-item1-info-qty'>
+              <p>Quantity: 1</p>
+            </div>
+            <div className='cart-carousel-item1-info-minus'>
+              <button>-</button>
+            </div>
+            <div className='cart-carousel-item1-info-plus'>
+              <button>+</button>
+            </div>
+            <div className='cart-carousel-item1-info-rm'>
+              <button>Remove</button>
+            </div>
+          </div>
+        </div>
+        <div className='cart-carousel-r-arrow'></div>
       </div>
     </div>
   );
