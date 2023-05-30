8d877e8448e64cdf0875ded4b0953249dd647d30
ky-lan <113309181+ky-lan@users.noreply.github.com>
2023-05-30 18:02:22 -0400

    finished cart page, clean script, then test the website

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Cart.scss';
+import { Link } from 'react-router-dom';
 
 export default function Cart() {
   return (
@@ -24,7 +25,7 @@ export default function Cart() {
         </div>
         <div className='cart-carousel-item1'>
           <div className='cart-carousel-item1-img'>
-            <img src="images/Pepperm_to_Lemon1.png" className="cart-carousel-item1-img-class" alt="Peppermint to Lemon" width="332" height="261" />
+            <img src="images/Pepperm_to_Lemon1.png" className="cart-carousel-item1-img-class" alt="Peppermint to Lemon" width="266" height="209" />
           </div>
           <div className='cart-carousel-item1-info'>
             <div className='cart-carousel-item1-info-price'>
@@ -44,7 +45,70 @@ export default function Cart() {
             </div>
           </div>
         </div>
-        <div className='cart-carousel-r-arrow'></div>
+        <div className='cart-carousel-item2'>
+          <div className='cart-carousel-item2-img'>
+            <img src="images/Banana_to_Pepperm1.png" className="cart-carousel-item2-img-class" alt="Peppermint to Lemon" width="266" height="209" />
+          </div>
+          <div className='cart-carousel-item2-info'>
+            <div className='cart-carousel-item2-info-price'>
+              <p>$1.99</p>
+            </div>
+            <div className='cart-carousel-item2-info-qty'>
+              <p>Quantity: 1</p>
+            </div>
+            <div className='cart-carousel-item2-info-minus'>
+              <button>-</button>
+            </div>
+            <div className='cart-carousel-item2-info-plus'>
+              <button>+</button>
+            </div>
+            <div className='cart-carousel-item2-info-rm'>
+              <button>Remove</button>
+            </div>
+          </div>
+        </div>
+        <div className='cart-carousel-item3'>
+          <div className='cart-carousel-item3-img'>
+            <img src="images/Cherry_to_Lemon1.png" className="cart-carousel-item3-img-class" alt="Peppermint to Lemon" width="266" height="209" />
+          </div>
+          <div className='cart-carousel-item3-info'>
+            <div className='cart-carousel-item3-info-price'>
+              <p>$1.99</p>
+            </div>
+            <div className='cart-carousel-item3-info-qty'>
+              <p>Quantity: 1</p>
+            </div>
+            <div className='cart-carousel-item3-info-minus'>
+              <button>-</button>
+            </div>
+            <div className='cart-carousel-item3-info-plus'>
+              <button>+</button>
+            </div>
+            <div className='cart-carousel-item3-info-rm'>
+              <button>Remove</button>
+            </div>
+          </div>
+        </div>
+        <div className='cart-carousel-r-arrow'>
+          <img src="images/R-arrow.png" className="cart-carousel-r-arrow-class" alt="Next" width="26" height="42" />
+        </div>
+      </div>
+
+      <div className='cart-subtotal'>
+        <p>Subtotal: $11.94</p>
+      </div>
+      <div className='cart-disclaimer'>
+        <p>Applicable taxes and shipping fees will be calculated at checkout.</p>
+      </div>
+      <div className='cart-keepshopping'>
+        <Link to='/'>
+          <button>Keep Shopping</button>
+        </Link>
+      </div>
+      <div className='cart-checkout'>
+        <Link to='/'>
+          <button>Checkout</button>
+        </Link>
       </div>
     </div>
   );
