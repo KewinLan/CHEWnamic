b72e414e84bba905cc2412fbc32212e4be7fe3b3
ky-lan <113309181+ky-lan@users.noreply.github.com>
2023-08-03 12:16:36 -0400

    D:\Fanshawe\Portfolio\CHEWnamic\CHEWnamic_code\CHEWnamic

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -14,6 +14,11 @@ export default function Home() {
             <button>Custom-made flavours</button>
           </Link>
         </div>
+        <div className='home-pre-made-gum-btn1'>
+          <Link to="/custom">
+            <button>Pre-made gum</button>
+          </Link>
+        </div>
         <div className='home-about-us-btn1'>
           <Link to="/about">
             <button>About us</button>
@@ -23,13 +28,14 @@ export default function Home() {
       <div className='home-products-area-top'>
         <div className='gum-images-row1'>
           <div className="banana-cherry-img-link">
-            <img src="images/Banana_to_Cherry1.png" className="banana-cherry-img" alt="Orange Mango Bottle" width="272" height="208" />
+            <img src="images/Banana_to_Cherry1.png" className="banana-cherry-img" alt="Orange Mango Bottle" width="320" height="248" />
+            {/* for desktop this should be width="272" height="208" */}
           </div>
           <div className="peppermint-lemon-img-link">
-            <img src="images/Pepperm_to_Lemon1.png" className="peppermint-lemon-img" alt="Apple Strawberry Bottle" width="272" height="208" />
+            <img src="images/Pepperm_to_Lemon1.png" className="peppermint-lemon-img" alt="Apple Strawberry Bottle" width="320" height="248" />
           </div>
           <div className="raspberry-peppermint-img-link">
-            <img src="images/Rasp_to_Pepperm1.png" className="raspberry-peppermint-img" alt="Mango Strawberry Bottle" width="272" height="208" />
+            <img src="images/Rasp_to_Pepperm1.png" className="raspberry-peppermint-img" alt="Mango Strawberry Bottle" width="320" height="248" />
           </div>
         </div>
         <div className='names-of-gum-row1'>
@@ -60,13 +66,13 @@ export default function Home() {
     <div className='home-bg'></div>
       <div className='gum-images-row2'>      
         <div className="cherry-lemon-img-link">
-          <img src="images/Cherry_to_Lemon1.png" className="cherry-lemon-img" alt="Apple Grape Bottle" width="272" height="208" />
+          <img src="images/Cherry_to_Lemon1.png" className="cherry-lemon-img" alt="Apple Grape Bottle" width="320" height="248" />
         </div>
         <div className="banana-peppermint-img-link">
-          <img src="images/Banana_to_Pepperm1.png" className="banana-peppermint-img" alt="Grape Strawberry Bottle" width="272" height="208" />
+          <img src="images/Banana_to_Pepperm1.png" className="banana-peppermint-img" alt="Grape Strawberry Bottle" width="320" height="248" />
         </div>
         <div className="peppermint-cherry-img-link">
-          <img src="images/Pepperm_to_Cherry1.png" className="peppermint-cherry-img" alt="Grape Strawberry Bottle" width="272" height="208" />
+          <img src="images/Pepperm_to_Cherry1.png" className="peppermint-cherry-img" alt="Grape Strawberry Bottle" width="320" height="248" />
         </div>
       </div>
       <div className='names-of-gum-row2'>
@@ -93,6 +99,12 @@ export default function Home() {
           <button>Explore</button>
         </div>
       </div>
+
+      <div className='home-custom-made-btn-mobile'>
+          <Link to="/custom">
+            <button>Custom-made flavours</button>
+          </Link>
+        </div>
     </>
   );
 };
