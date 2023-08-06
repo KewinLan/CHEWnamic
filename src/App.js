00780b95003c9b373923ec2e2a0c38ea98604c1c
ky-lan <113309181+ky-lan@users.noreply.github.com>
2023-08-05 21:52:44 -0400

    footer is not fixed, things are still going off page.

--- src/App.js
+++ src/App.js
@@ -40,3 +40,4 @@ export default App;
 //v029 3 Aug 2023 2:03 pm: I had to do a git pull (likely because I manually updated the README on 30 May). Today I am working on changing it to a mobile-first website
 //v030 4 Aug 11 pm: continue working on mobile version
 //v031 5 Aug 10:46 am: mobile home page not tall enough, so elements are falling off the page
+//v032 9:52 pm: footer is not fixed, things are still going off page.
